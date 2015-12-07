b1bb611eccd6b2d92a732f2774ab0d8a7b74bd1a
phesami <p.hesami@gmail.com>
2015-12-06 23:46:52 -0800

    Adding HW5

---
+++ DSE200/day_5_mining_the_Social_web/exercises/Twitter_Keys.py
@@ -0,0 +1,6 @@
+def getkeys():
+    dictionary={"access_token_key": "4352013854-olp2gMsokNfhg32xEnjhoK8hiWrKmX6YadxLrCz", "api_secret": "2KLkaKuYckLTxORwEALL1JHtjtyz1kcIQsvmF7BO2vd59yAzOu", "api_key": "cINBZ0PpEzME7IWpr4l619NyF", "access_token_secret": "OYiYqNgiXwW1r7PacvtepEEeawXInCT6hqZa6Uffxf1xp"}
+    return dictionary.values()
+def getdict():
+    dictionary={"access_token_key": "4352013854-olp2gMsokNfhg32xEnjhoK8hiWrKmX6YadxLrCz", "api_secret": "2KLkaKuYckLTxORwEALL1JHtjtyz1kcIQsvmF7BO2vd59yAzOu", "api_key": "cINBZ0PpEzME7IWpr4l619NyF", "access_token_secret": "OYiYqNgiXwW1r7PacvtepEEeawXInCT6hqZa6Uffxf1xp"}
+    return dictionary
